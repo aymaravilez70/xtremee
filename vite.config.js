8c9a3ff4ac9713bc9eafc2a0e5be81b3a8df0468
Aymar <aymaravilez70@gmail.com>
2026-05-08 11:20:14 -0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Website-Project-UEES/",
+  base: "/xtremee/",
   build: {
     rollupOptions: {
       input: {
